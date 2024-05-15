fea2eeccbb314ea3b1ed83aa68f68a511845ec20
Jonathan Shimwell <mail@jshimwell.com>
2024-05-16 00:34:25 +0100

    added project

---
+++ examples/usage.py
@@ -0,0 +1,3 @@
+import example_python_package_with_rust_backend
+
+example_python_package_with_rust_backend.hello_world(1,2.01, 'hi')
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn hello_world(int_arg: i32, float_arg: f32, str_arg: &str) -> PyResult<i32> {
+    println!("float_arg {}", float_arg);
+    println!("int_arg {}", int_arg);
+    println!("str_arg {}", str_arg);
+    
+    Ok(int_arg * int_arg)
+}
+
+
+#[pymodule]
+fn example_python_package_with_rust_backend(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(hello_world, m)?)?;
+    Ok(())
+}
